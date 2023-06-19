f6700ba6f55cfaeff9b8016853513b51a6caaf4d
NhanNguyen <nnguyen14@sfsu.edu>
2023-06-18 23:54:05 -0700

    handle distinct and reverse

--- dictionaryApplication/InteractiveDictionary.java
+++ dictionaryApplication/InteractiveDictionary.java
@@ -21,33 +21,42 @@ public class InteractiveDictionary {
     }
 
     // Search the dictionary for matching entries based on the search key and part of speech
-    public List<EnumDictionaryData> search(String searchKey, String partOfSpeech) {
-        List<EnumDictionaryData> matchingEntries = new ArrayList<>();
+    public List<String> search(String searchKey, String partOfSpeech, boolean isDistinct, boolean isReverse) {
+        List<String> matchingEntries = new ArrayList<>();
         String searchKeyLower = searchKey.toLowerCase();
 
         if (dictionary.containsKey(searchKeyLower)) {
             List<EnumDictionaryData> entries = dictionary.get(searchKeyLower);
             if (partOfSpeech == null) {
-                matchingEntries.addAll(entries);
+                for (EnumDictionaryData entry : entries) {
+                    matchingEntries.add(entries.toString());
+                }
             } else {
                 for (EnumDictionaryData entry : entries) {
                     if (entry.getPartOfSpeech().equalsIgnoreCase(partOfSpeech)) {
-                        matchingEntries.add(entry);
+                        matchingEntries.add(entry.toString());
                     }
                 }
+            }
+            if (isDistinct) {
+                matchingEntries = new ArrayList<>(new HashSet<>(matchingEntries));
+            }
+            Collections.sort(matchingEntries);
+            if (isReverse) {
+                Collections.reverse(matchingEntries);
             }
         }
         return matchingEntries;
     }
 
     // Display the search results
-    public void displayResults(List<EnumDictionaryData> entries) {
+    public void displayResults(List<String> entries) {
         if (entries.isEmpty()) {
             System.out.println("<NOT FOUND> To be considered for the next release. Thank you.");
         } else {
             System.out.println("|");
-            for (EnumDictionaryData entry : entries) {
-                System.out.println(entry.toString());
+            for (String entry : entries) {
+                System.out.println(entry);
             }
             System.out.println("|");
         }
