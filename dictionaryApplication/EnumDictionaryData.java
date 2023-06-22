6a059aee67ac7ae645e4a4dfe3fc7dd74a5cf92b
NhanNguyen <nnguyen14@sfsu.edu>
2023-06-21 22:36:31 -0700

    update Enum classes

--- dictionaryApplication/EnumDictionaryData.java
+++ dictionaryApplication/EnumDictionaryData.java
@@ -1,5 +1,5 @@
 // Enum to store the dictionary data
-enum EnumDictionaryData {
+public enum EnumDictionaryData { //can try another data type for enum data and constructor later
     ARROW_NOUN_1("Arrow", EnumPartOfSpeech.NOUN.getPartOfSpeech(), "Here is one arrow: <IMG> -=>> </IMG>"),
     BOOK_NOUN_1("Book", EnumPartOfSpeech.NOUN.getPartOfSpeech(), "A set of pages."),
     BOOK_NOUN_2("Book", EnumPartOfSpeech.NOUN.getPartOfSpeech(), "A written work published in printed or electronic form."),
@@ -41,7 +41,7 @@ enum EnumDictionaryData {
     private final String partOfSpeech;
     private final String definition;
 
-    EnumDictionaryData(String keyword, String partOfSpeech, String definition) {
+    private EnumDictionaryData(String keyword, String partOfSpeech, String definition) {
         this.keyword = keyword;
         this.partOfSpeech = partOfSpeech;
         this.definition = definition;
